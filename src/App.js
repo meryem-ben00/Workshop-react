fbeb3ac8e001d2c2f03b694023bde04a7cb89e25
khalidismail99 <148463171+khalidismail99@users.noreply.github.com>
2023-10-27 10:01:23 +0100

    Clean App.js

--- src/App.js
+++ src/App.js
@@ -5,50 +5,21 @@ import Users from "./pages/Users";
 import Patients from "./pages/Patients";
 import Test from "./pages/Test";
 import Osama from "./pages/Osama";
-import Principale from "./pages/addDoctor/Principale.jsx";
-import SignIn from "./pages/sign/SignIn";
-import SignUp from "./pages/sign/SignUp";
-
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        BrowserRouter >
-        <
-        Navbare / >
-        <
-        Routes >
-        <
-        Route exact path = "/"
-        element = { < Dashboard / > }
-        />{" "} <
-        Route path = "/users/*"
-        element = { < Users / > }
-        />{" "} <
-        Route path = "/patients"
-        element = { < Patients / > }
-        />{" "} <
-        Route path = "/test"
-        element = { < Test / > }
-        />{" "} <
-        Route path = "/osama"
-        element = { < Osama / > }
-        /> <
-        Route path = "/users/addDoctor"
-        element = { < Principale / > }
-        />{" "} <
-        Route path = "/SignIn"
-        element = { < SignIn / > }
-        />{" "} <
-        Route path = "/SignUp"
-        element = { < SignUp / > }
-        />
-
-        <
-        /Routes> <
-        /BrowserRouter> <
-        /div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbare />
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route path="/users/*" element={<Users />} />
+          <Route path="/patients" element={<Patients />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/osama" element={<Osama />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
