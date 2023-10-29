c76e9f9eea7f37910a96ac1b95d90513c903ba32
Abdelwarit <kassimi.aabbdo@gmail.com>
2023-10-29 20:34:01 +0100

    code clean up

--- src/pages/addDoctor/Details.jsx
+++ src/pages/addDoctor/Details.jsx
@@ -1,45 +1,39 @@
 import React from "react";
 
 export default function Details() {
-    return ( <
-        div >
-        <
-        div className = "flex justify-center items-start w-full gap-8 flex-grow max-w-screen-lg mx-auto " >
-        <
-        div className = "flex justify-items-start   w-[50%] h-full mb-1" >
-        <
-        div className = "flex flex-col w-[100%] " >
-        <
-        label className = "font-normal text-xs text-[#5F8D4E] pb-1" >
-        First name <
-        /label> <
-        input placeholder = "Your first name"
-        className = "w-[100%] h-10 text-white  bg-[#1E2E5C] border-[1px] border-solid border-white rounded outline-none px-4 py-1 mb-7" /
-        >
-        <
-        label className = "font-normal text-xs text-[#5F8D4E] pb-1" >
-        Last name <
-        /label> <
-        input placeholder = "Your last name"
-        className = "w-[100%] h-10 text-white  bg-[#1E2E5C] border-[1px] border-solid border-white rounded outline-none px-4 py-1 mb-7" /
-        >
-        <
-        /div> <
-        /div> <
-        div className = "flex justify-items-start   w-[50%] h-full mb-1" >
-        <
-        div className = "flex flex-col w-[100%] h-full " >
-        <
-        label className = "font-normal text-xs text-[#5F8D4E] pb-1" >
-        Designation <
-        /label> <
-        input placeholder = "Your first name"
-        className = "w-[100%] h-10 text-white  bg-[#1E2E5C] border-[1px] border-solid border-white rounded outline-none px-4 py-1 mb-7" /
-        >
-        <
-        /div> <
-        /div> <
-        /div> <
-        /div>
-    );
+  return (
+    <div>
+      <div className="flex justify-center items-start w-full gap-8 flex-grow max-w-screen-lg mx-auto ">
+        <div className="flex justify-items-start   w-[50%] h-full mb-1">
+          <div className="flex flex-col w-[100%] ">
+            <label className="font-normal text-xs text-[#5F8D4E] pb-1">
+              First name{" "}
+            </label>{" "}
+            <input
+              placeholder="Your first name"
+              className="w-[100%] h-10 text-white  bg-[#1E2E5C] border-[1px] border-solid border-white rounded outline-none px-4 py-1 mb-7"
+            />
+            <label className="font-normal text-xs text-[#5F8D4E] pb-1">
+              Last name{" "}
+            </label>{" "}
+            <input
+              placeholder="Your last name"
+              className="w-[100%] h-10 text-white  bg-[#1E2E5C] border-[1px] border-solid border-white rounded outline-none px-4 py-1 mb-7"
+            />
+          </div>{" "}
+        </div>{" "}
+        <div className="flex justify-items-start   w-[50%] h-full mb-1">
+          <div className="flex flex-col w-[100%] h-full ">
+            <label className="font-normal text-xs text-[#5F8D4E] pb-1">
+              Designation{" "}
+            </label>{" "}
+            <input
+              placeholder="Your first name"
+              className="w-[100%] h-10 text-white  bg-[#1E2E5C] border-[1px] border-solid border-white rounded outline-none px-4 py-1 mb-7"
+            />
+          </div>{" "}
+        </div>{" "}
+      </div>{" "}
+    </div>
+  );
 }
--- src/pages/addDoctor/Headers.jsx
+++ src/pages/addDoctor/Headers.jsx
@@ -1,72 +1,80 @@
 import React from "react";
 export default function Headers({ step, move_between_li }) {
-    const stepTitles = {
-        1: "Basic Information",
-        2: "Enter details",
-        3: "Select services",
-    };
-    return ( <
-        div className = "absolute top-32 z-10 " >
-        <
-        ul className = "flex justify-center items-center w-full gap-8 mb-[50px] max-w-screen-lg mx-auto p-4" >
-        <
-        li className = { `border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer  ${
+  const stepTitles = {
+    1: "Basic Information",
+    2: "Enter details",
+    3: "Select services",
+  };
+  return (
+    <div className="absolute top-32 z-10 ">
+      <ul className="flex justify-center items-center w-full gap-8 mb-[50px] max-w-screen-lg mx-auto p-4">
+        <li
+          className={`border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer  ${
             step === 1 ? "border-[#5F8D4E] text-[#5F8D4E]" : "text-[#C9C9C9]"
-          }` }
-        onClick = {
-            () => move_between_li(1) } >
-        <
-        span className = { `text-xl font-semibold leading-10 ${
+          }`}
+          onClick={() => move_between_li(1)}
+        >
+          <span
+            className={`text-xl font-semibold leading-10 ${
               step === 1 ? "text-[#5F8D4E]" : "text-[#C9C9C9]"
-            }` } >
-        1 <
-        /span>
-        Basic Information <
-        /li> <
-        li className = { `border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer ${
+            }`}
+          >
+            1{" "}
+          </span>
+          Basic Information{" "}
+        </li>{" "}
+        <li
+          className={`border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer ${
             step === 2 ? "border-[#5F8D4E] text-[#5F8D4E]" : "text-[#C9C9C9]"
-          }` }
-        onClick = {
-            () => move_between_li(2) } >
-        <
-        span className = { `text-xl font-semibold leading-10  ${
+          }`}
+          onClick={() => move_between_li(2)}
+        >
+          <span
+            className={`text-xl font-semibold leading-10  ${
               step === 2 ? "text-[#5F8D4E]" : "text-[#C9C9C9]"
-            }` } >
-        2 <
-        /span>
-        Enter details <
-        /li> <
-        li className = { `border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer ${
+            }`}
+          >
+            2{" "}
+          </span>
+          Enter details{" "}
+        </li>{" "}
+        <li
+          className={`border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer ${
             step === 3 ? "border-[#5F8D4E] text-[#5F8D4E]" : "text-[#C9C9C9]"
-          }` }
-        onClick = {
-            () => move_between_li(3) } >
-        <
-        span className = { `text-xl font-semibold leading-10 ${
+          }`}
+          onClick={() => move_between_li(3)}
+        >
+          <span
+            className={`text-xl font-semibold leading-10 ${
               step === 3 ? "text-[#5F8D4E]" : "text-[#C9C9C9]"
-            }` } >
-        3 <
-        /span>
-        Select services <
-        /li> <
-        li className = { `border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer ${
+            }`}
+          >
+            3{" "}
+          </span>
+          Select services{" "}
+        </li>{" "}
+        <li
+          className={`border-b-2 border-solid w-[250.21px] text-sm font-semibold cursor-pointer ${
             step === 4 ? "border-[#5F8D4E] text-[#5F8D4E]" : "text-[#C9C9C9]"
-          }` }
-        onClick = {
-            () => move_between_li(4) } >
-        <
-        span className = { `text-xl font-semibold leading-10 ${
+          }`}
+          onClick={() => move_between_li(4)}
+        >
+          <span
+            className={`text-xl font-semibold leading-10 ${
               step === 4 ? "text-[#5F8D4E]" : "text-[#C9C9C9]"
-            }` } >
-        4 <
-        /span>
-        Review and submit <
-        /li> <
-        /ul> <
-        div className = "max-w-screen-lg mx-auto p-4 w-full " >
-        <
-        h3 className = "font-medium text-base text-white" > { stepTitles[step] } < /h3> <
-        /div> <
-        /div>
-    );
+            }`}
+          >
+            4{" "}
+          </span>
+          Review and submit{" "}
+        </li>{" "}
+      </ul>{" "}
+      <div className="max-w-screen-lg mx-auto p-4 w-full ">
+        <h3 className="font-medium text-base text-white">
+          {" "}
+          {stepTitles[step]}{" "}
+        </h3>{" "}
+      </div>{" "}
+    </div>
+  );
 }
